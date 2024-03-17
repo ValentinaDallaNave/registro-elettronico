7bae1db99d154e9294d885546cf6d9dce2b0a7bc
MirkoMaralit <148321087+MirkoMaralit@users.noreply.github.com>
2024-03-17 12:40:38 +0000

    chiamate al server con async await

--- public/remote.js
+++ public/remote.js
@@ -1,49 +1,36 @@
 //chiamate al server
-const login = (tipo, username, password) => {
-  return new Promise((res, rej) => {
-    fetch("/login/" + tipo, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-    }).then((response) => {
-      // restituisce true solo se lo stato della risposta è diverso da Unauthorized (401)
-      // restituisce false se lo stato è Unauthorized (401)
-      res(response.status === 401 ? false : true)});
+const login = async (tipo, username, password) => {
+  let response = await fetch("/login/" + tipo, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
   });
+  // restituisce true solo se lo stato della risposta è diverso da Unauthorized (401)
+  // restituisce false se lo stato è Unauthorized (401)
+  return response.status === 401 ? false : true;
 };
 
-const send = (url, data, credentials) => {
+const send = async (url, data, credentials) => {
   // Per ogni servizio con metodo Post servono le credenziali admin o prof
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        username: credentials.username,
-        password: credentials.password,
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        resolve(json);
-      });
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      username: credentials.username,
+      password: credentials.password,
+    },
+    body: JSON.stringify(data),
   });
+  return response.json();
 };
 
-const load = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        resolve(json);
-      });
-  });
+const load = async (url) => {
+  return (await fetch(url)).json();
 };
 
 export { login, send, load };
